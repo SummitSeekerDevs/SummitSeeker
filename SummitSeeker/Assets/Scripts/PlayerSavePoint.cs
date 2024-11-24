e3cfef440e1699e6dec98c16d0bd3ed97656f34d
KennethBurgen <kennethjlburgen@gmail.com>
2024-11-24 21:02:25 +0100

    Projekt initialisiert

---
+++ SummitSeeker/Assets/Scripts/PlayerSavePoint.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerSavePoint : MonoBehaviour
+{
+    public Transform activeSavePoint { get; private set;}
+    public KeyCode resetToActiveSavePoint = KeyCode.R;
+
+    public void setActiveSavePoint(Transform savePoint) {
+        activeSavePoint = savePoint;
+    }
+
+    private void FixedUpdate() {
+        if (activeSavePoint != null && Input.GetKey(resetToActiveSavePoint)) {
+            GetComponent<Rigidbody>().position = activeSavePoint.position;
+            activeSavePoint = null;
+        }
+    }
+}
---
+++ SummitSeeker/Assets/Scripts/ThrowingKnife.cs
@@ -0,0 +1,66 @@
+using UnityEngine;
+
+public class ThrowingKnife : MonoBehaviour
+{
+    [Header("References")]
+    public Transform cam, attackPoint;
+    public GameObject objectToThrow;
+
+    [Header("Settings")]
+    public int totalThrows;
+    public float throwCooldown;
+
+    [Header("Throwing")]
+    public KeyCode throwKey = KeyCode.Mouse0;
+    public float throwForce, throwUpwardForce;
+
+    bool readyToThrow;
+
+    private void Start() {
+        readyToThrow = true;
+    }
+
+    private void Update() {
+        if(Input.GetKeyDown(throwKey) && readyToThrow && totalThrows > 0) {
+            Throw();
+        }
+    }
+
+    private void Throw() {
+        readyToThrow = false;
+
+        // instantiate object to throw
+        GameObject projectile = Instantiate(objectToThrow, attackPoint.position, cam.rotation);
+
+        // get rigidbody component
+        Rigidbody projectileRb = projectile.GetComponent<Rigidbody>();
+
+        projectileRb.angularDamping = 0f;
+
+        // calculate direction
+        Vector3 forceDirection = cam.transform.forward;
+
+        RaycastHit hit;
+
+        if (Physics.Raycast(cam.position, cam.forward, out hit, 500f)) {
+            forceDirection = (hit.point - attackPoint.position).normalized;
+        }
+
+        // add force
+        Vector3 forceToAdd = forceDirection * throwForce + transform.up * throwUpwardForce;
+        projectileRb.AddForce(forceToAdd, ForceMode.Impulse);
+
+        // Drehung
+        Vector3 torque = attackPoint.transform.right * 1000 * Time.deltaTime;
+        projectileRb.AddTorque(torque, ForceMode.Impulse);
+
+        totalThrows--;
+
+        // implement throwCooldown
+        Invoke(nameof(ResetThrow), throwCooldown);
+    }
+
+    private void ResetThrow() {
+        readyToThrow = true;
+    }
+}
