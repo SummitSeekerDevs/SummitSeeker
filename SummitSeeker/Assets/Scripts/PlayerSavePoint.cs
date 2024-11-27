5adbb66bb756ba2ff08f7f7d4c23dcf57a343696
KennethBurgen <kennethjlburgen@gmail.com>
2024-11-27 17:39:54 +0100

    #13 Input Action Map hinzugefügt und PlayerSavePoint Skript angepasst

--- SummitSeeker/Assets/Scripts/PlayerSavePoint.cs
+++ SummitSeeker/Assets/Scripts/PlayerSavePoint.cs
@@ -1,18 +1,33 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerSavePoint : MonoBehaviour
 {
     public Transform activeSavePoint { get; private set;}
-    public KeyCode resetToActiveSavePoint = KeyCode.R;
+    public KeyCode resetToActiveSavePointKey = KeyCode.R;
+
+    private PlayerInput_Actions _playerInputActions;
+
+    private void Awake() {
+        _playerInputActions = new PlayerInput_Actions();
+    }
+
+    private void OnEnable() {
+        _playerInputActions.Player.Enable();
+        _playerInputActions.Player.ResetToSavePoint.performed += OnResetToSavePoint;
+    }
+
+    private void OnDisable() {
+        _playerInputActions.Player.ResetToSavePoint.performed -= OnResetToSavePoint;
+        _playerInputActions.Player.Disable();
+    }
 
     public void setActiveSavePoint(Transform savePoint) {
         activeSavePoint = savePoint;
     }
 
-    private void FixedUpdate() {
-        if (activeSavePoint != null && Input.GetKey(resetToActiveSavePoint)) {
+    private void OnResetToSavePoint(InputAction.CallbackContext context) {
+        if (activeSavePoint != null) {
             GetComponent<Rigidbody>().position = activeSavePoint.position;
             activeSavePoint = null;
         }
