1f9722b42fbfe83bfdecf37104b8e0a8636fdf4d
KennethBurgen <147069208+KennethBurgen@users.noreply.github.com>
2025-03-29 14:53:05 +0100

    PlayerMovementTest File hinzugefügt

---
+++ SummitSeeker/Assets/Tests/PlayerMovementTest.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+public class PlayerMovementTest
+{
+    private Keyboard keyboard;
+
+    [UnityTest]
+    public IEnumerator MovePlayerTest()
+    {
+        yield return null;
+    }
+}
