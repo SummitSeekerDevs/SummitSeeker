af0ffab982929f0a0ea4d9501388efecdb166257
KennethBurgen <147069208+KennethBurgen@users.noreply.github.com>
2025-04-07 22:04:31 +0200

    Umstieg auf Verwendung von rb und Funktionalität von CallablePlatform in Methoden ausgelagert

--- Assets/Scripts/CallablePlatform.cs
+++ Assets/Scripts/CallablePlatform.cs
@@ -1,27 +1,55 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class CallablePlatform : MonoBehaviour
 {
     public Transform targetPosition;
     public float moveSpeed = 2f;
     public bool moveToPosition = false;
+    internal float distanceToTarget = 0.1f;
+
+    Rigidbody rb;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        rb.isKinematic = true;
+    }
 
     private void FixedUpdate()
     {
         if (moveToPosition)
         {
-            transform.position = Vector3.MoveTowards(
-                transform.position,
-                targetPosition.position,
-                moveSpeed * Time.fixedDeltaTime
-            );
+            MovePlatform();
+            TargetPointDistanceCheckSetNextTarget();
+        }
+    }
 
-            if (Vector3.Distance(transform.position, targetPosition.position) < 0.1f)
-            {
-                moveToPosition = false;
-            }
+    private void MovePlatform()
+    {
+        Vector3 nextPos = getNextPositionTowardsTarget();
+        rb.MovePosition(nextPos);
+    }
+
+    internal Vector3 getNextPositionTowardsTarget()
+    {
+        Vector3 currentPosition = rb.position;
+        Vector3 nextPosition = Vector3.MoveTowards(
+            currentPosition,
+            targetPosition.position,
+            moveSpeed * Time.fixedDeltaTime
+        );
+
+        return nextPosition;
+    }
+
+    private void TargetPointDistanceCheckSetNextTarget()
+    {
+        if (Vector3.Distance(rb.position, targetPosition.position) < distanceToTarget)
+        {
+            moveToPosition = false;
         }
     }
 }
