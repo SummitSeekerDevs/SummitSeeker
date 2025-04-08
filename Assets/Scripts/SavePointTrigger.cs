92ae6e4dd1104820b589b7ab8f271117d20b3599
KennethBurgen <147069208+KennethBurgen@users.noreply.github.com>
2025-04-08 18:37:57 +0200

    SavePointTriggerScript Sichtbarkeiten der Vars geändert

--- Assets/Scripts/SavePointTrigger.cs
+++ Assets/Scripts/SavePointTrigger.cs
@@ -1,16 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 using UnityEngine;
+
+[assembly: InternalsVisibleTo("Tests")]
 
 public class SavePointTrigger : MonoBehaviour
 {
-    public Transform savePoint;
-    public PlayerSavePoint playerSavePoint;
+    [SerializeField]
+    private Transform savePoint;
+    private PlayerSavePoint playerSavePoint;
     private bool usedSavePoint = false;
+    public string colliderTag;
+
+    private void Start()
+    {
+        playerSavePoint = GameManager.Instance.playerGO.GetComponent<PlayerSavePoint>();
+    }
 
     private void OnTriggerEnter(Collider other)
     {
-        if (!usedSavePoint)
+        if (!usedSavePoint && other.transform.CompareTag(colliderTag))
         {
             Debug.Log("1 use savepoint unlocked");
             playerSavePoint.setActiveSavePoint(savePoint);
