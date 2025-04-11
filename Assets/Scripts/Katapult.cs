f438faea6aeb17f7247f052249da634a3ebbad7b
KennethBurgen <147069208+KennethBurgen@users.noreply.github.com>
2025-04-11 23:01:32 +0200

    Redifined and reworked Katapult logic

--- Assets/Scripts/Katapult.cs
+++ Assets/Scripts/Katapult.cs
@@ -6,16 +6,36 @@ public class Katapult : MonoBehaviour
 {
     Rigidbody playerRb;
     public float shootUpForce = 50f;
+    private RigidbodyConstraints playerRbDefaultconstraints;
 
-    private void OnCollisionEnter(Collision other)
+    private void OnCollisionEnter(Collision collision)
     {
-        playerRb = other.transform.GetComponent<Rigidbody>();
+        playerRb = collision.transform.GetComponent<Rigidbody>();
+        playerRbDefaultconstraints = playerRb.constraints;
+
+        ToggleFreezePlayerPosition(playerRb, true);
+
+        // Set timer
         Invoke(nameof(shootPlayerUp), 4f);
+    }
+
+    private void ToggleFreezePlayerPosition(Rigidbody playerRb, bool freeze)
+    {
+        if (freeze)
+        {
+            playerRb.constraints = RigidbodyConstraints.FreezeAll;
+        }
+        else
+        {
+            playerRb.constraints = playerRbDefaultconstraints;
+        }
     }
 
     // BRAUCHt check ob player noch drauf ist
     private void shootPlayerUp()
     {
+        ToggleFreezePlayerPosition(playerRb, false);
+
         playerRb.AddForce(transform.up * shootUpForce, ForceMode.Impulse);
     }
 }
