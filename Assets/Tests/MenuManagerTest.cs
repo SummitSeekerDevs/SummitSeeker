ebacc07bc8318ff5116b662639af347d14c6c24b
KennethBurgen <147069208+KennethBurgen@users.noreply.github.com>
2025-04-27 21:22:05 +0200

    MenuManagerTest Referenzen gesetzt

--- Assets/Tests/MenuManagerTest.cs
+++ Assets/Tests/MenuManagerTest.cs
@@ -5,12 +5,13 @@ using UnityEngine.TestTools;
 
 public class MenuManagerTest
 {
-    private GameObject menuManager;
+    private GameObject menuManager,
+        gameManager;
 
     private int MENU_FPS = 30,
         MENU_VSYNC_MODE = 0;
 
-    private void setGameManager()
+    private void setMenuManager()
     {
         if (MenuManager.Instance != null)
         {
@@ -22,10 +23,43 @@ public class MenuManagerTest
         }
     }
 
+    private void setGameManager()
+    {
+        if (GameManager.Instance != null)
+        {
+            gameManager = GameManager.Instance.gameObject;
+        }
+        else
+        {
+            gameManager = GameObject.Instantiate(Resources.Load<GameObject>("Prefabs/GameManager"));
+        }
+    }
+
     [SetUp]
     public void SetUp()
     {
         setGameManager();
+        setMenuManager();
+
+        // create and set Panels
+        MenuManager.Instance.MenuPanel = new GameObject("menuPanel");
+        MenuManager.Instance.NewsPanel = new GameObject("newsPanel");
+        MenuManager.Instance.CreditsPanel = new GameObject("creditsPanel");
+        MenuManager.Instance.MainMenuButtons = new GameObject("mainMenuButtons");
+        MenuManager.Instance.SettingsMenuButtons = new GameObject("settingsMenuButtons");
+    }
+
+    [TearDown]
+    public void TearDown()
+    {
+        Object.Destroy(MenuManager.Instance.MenuPanel);
+        Object.Destroy(MenuManager.Instance.NewsPanel);
+        Object.Destroy(MenuManager.Instance.CreditsPanel);
+        Object.Destroy(MenuManager.Instance.MainMenuButtons);
+        Object.Destroy(MenuManager.Instance.SettingsMenuButtons);
+
+        Object.Destroy(gameManager);
+        Object.Destroy(menuManager);
     }
 
     [UnityTest]
@@ -51,27 +85,27 @@ public class MenuManagerTest
         );
     }
 
-    [UnityTest]
-    public IEnumerator CanUpdateMenuStateWithButtonTest()
+    [Test]
+    public void CanUpdateMenuStateWithButtonTest()
     {
         // Startup Menu
         MenuManager.Instance.ButtonUpdateMenuState(0);
 
-        yield return null;
+        //yield return null;
 
         Assert.AreEqual(MenuState.StartupMenu, MenuManager.Instance._currentState);
 
         // Options Menu
         MenuManager.Instance.ButtonUpdateMenuState(1);
 
-        yield return null;
+        //yield return null;
 
         Assert.AreEqual(MenuState.OptionsMenu, MenuManager.Instance._currentState);
 
         // Credits Menu
         MenuManager.Instance.ButtonUpdateMenuState(2);
 
-        yield return null;
+        //yield return null;
 
         Assert.AreEqual(MenuState.CreditsMenu, MenuManager.Instance._currentState);
     }
