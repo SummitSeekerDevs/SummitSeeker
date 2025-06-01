3abc64335a7ca637ec0be255e2e6b8104cec4c34
KennethBurgen <147069208+KennethBurgen@users.noreply.github.com>
2025-06-01 23:18:03 +0200

    MenuManager Tests angepasst

--- Assets/Tests/MenuManagerTest.cs
+++ Assets/Tests/MenuManagerTest.cs
@@ -5,111 +5,75 @@ using UnityEngine.TestTools;
 
 public class MenuManagerTest
 {
-    private GameObject menuManager,
-        gameManager;
-
-    private int MENU_FPS = 30,
-        MENU_VSYNC_MODE = 0;
-
-    private void setMenuManager()
-    {
-        if (MenuManager.Instance != null)
-        {
-            menuManager = MenuManager.Instance.gameObject;
-        }
-        else
-        {
-            menuManager = GameObject.Instantiate(Resources.Load<GameObject>("Prefabs/MenuManager"));
-        }
-    }
-
-    private void setGameManager()
-    {
-        if (GameManager.Instance != null)
-        {
-            gameManager = GameManager.Instance.gameObject;
-        }
-        else
-        {
-            gameManager = GameObject.Instantiate(Resources.Load<GameObject>("Prefabs/GameManager"));
-        }
-    }
+    private GameObject menuManager;
+    private MenuManager menuManagerScript;
 
     [SetUp]
     public void SetUp()
     {
-        setGameManager();
-        setMenuManager();
-
-        // create and set Panels
-        MenuManager.Instance.MenuPanel = new GameObject("menuPanel");
-        MenuManager.Instance.NewsPanel = new GameObject("newsPanel");
-        MenuManager.Instance.CreditsPanel = new GameObject("creditsPanel");
-        MenuManager.Instance.MainMenuButtons = new GameObject("mainMenuButtons");
-        MenuManager.Instance.SettingsMenuButtons = new GameObject("settingsMenuButtons");
+        menuManager = new GameObject("MenuManager");
+        menuManagerScript = menuManager.AddComponent<MenuManager>();
     }
 
     [TearDown]
     public void TearDown()
     {
-        Object.Destroy(MenuManager.Instance.MenuPanel);
-        Object.Destroy(MenuManager.Instance.NewsPanel);
-        Object.Destroy(MenuManager.Instance.CreditsPanel);
-        Object.Destroy(MenuManager.Instance.MainMenuButtons);
-        Object.Destroy(MenuManager.Instance.SettingsMenuButtons);
-
         Object.Destroy(menuManager);
     }
 
-    [UnityTest]
-    public IEnumerator MenuManagerInitTest()
+    [Test]
+    public void ButtonUpdateMenuStateTest()
     {
-        yield return new WaitForEndOfFrame();
-
-        GameManager.Instance.UpdateGameState(GameState.MainMenu);
-
-        yield return new WaitForSeconds(5f);
-
-        Assert.AreEqual(
-            MENU_FPS,
-            Application.targetFrameRate,
-            "Framerate is set to menu framerate"
-        );
-        Assert.AreEqual(
-            MENU_VSYNC_MODE,
-            QualitySettings.vSyncCount,
-            "Vsync mode is set to menu framerate"
-        );
-
+        // Startup Menu
+        MenuManager._instance.ButtonUpdateMenuState(0);
         Assert.AreEqual(
             MenuState.StartupMenu,
-            MenuManager.Instance._currentState,
-            "MenuState is startup State"
+            MenuManager._instance._currentState,
+            "Button update state to startup"
+        );
+
+        // Options Menu
+        MenuManager._instance.ButtonUpdateMenuState(1);
+        Assert.AreEqual(
+            MenuState.OptionsMenu,
+            MenuManager._instance._currentState,
+            "Button update state to options"
+        );
+
+        // Credits Menu
+        MenuManager._instance.ButtonUpdateMenuState(2);
+        Assert.AreEqual(
+            MenuState.CreditsMenu,
+            MenuManager._instance._currentState,
+            "Button update state to credits"
         );
     }
 
     [Test]
-    public void CanUpdateMenuStateWithButtonTest()
+    public void UpdateMenuStateTest()
     {
         // Startup Menu
-        MenuManager.Instance.ButtonUpdateMenuState(0);
-
-        //yield return null;
-
-        Assert.AreEqual(MenuState.StartupMenu, MenuManager.Instance._currentState);
+        MenuManager._instance.UpdateMenuState(MenuState.StartupMenu);
+        Assert.AreEqual(
+            MenuState.StartupMenu,
+            MenuManager._instance._currentState,
+            "Update state to startup"
+        );
 
         // Options Menu
-        MenuManager.Instance.ButtonUpdateMenuState(1);
-
-        //yield return null;
-
-        Assert.AreEqual(MenuState.OptionsMenu, MenuManager.Instance._currentState);
+        MenuManager._instance.UpdateMenuState(MenuState.OptionsMenu);
+        Assert.AreEqual(
+            MenuState.OptionsMenu,
+            MenuManager._instance._currentState,
+            "Update state to options"
+        );
 
         // Credits Menu
-        MenuManager.Instance.ButtonUpdateMenuState(2);
-
-        //yield return null;
-
-        Assert.AreEqual(MenuState.CreditsMenu, MenuManager.Instance._currentState);
+        MenuManager._instance.UpdateMenuState(MenuState.CreditsMenu);
+        Assert.AreEqual(
+            MenuState.CreditsMenu,
+            MenuManager._instance._currentState,
+            "Update state to credits"
+        );
     }
 }
