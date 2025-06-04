3d7f0d60017c5404f6e61c44bf05253539b9ec40
KennethBurgen <147069208+KennethBurgen@users.noreply.github.com>
2025-06-04 19:28:03 +0200

    InGameInstaller hinzugefügt

---
+++ Assets/Installers/InGameSceneInstaller.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+using Zenject;
+
+public class InGameSceneInstaller : MonoInstaller
+{
+    [SerializeField]
+    private GameObject _playerGameObject;
+
+    public override void InstallBindings()
+    {
+        Container.Bind<GameObject>().FromInstance(_playerGameObject).AsSingle().NonLazy();
+    }
+}
