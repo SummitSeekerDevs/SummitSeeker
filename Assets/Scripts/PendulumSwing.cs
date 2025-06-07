ec87218e7209b5990f384cb741841a3888c5e256
Viviane Johns <75577574+vivianejohns@users.noreply.github.com>
2025-06-07 21:00:06 +0200

    adress nitpick comments

--- Assets/Scripts/PendulumSwing.cs
+++ Assets/Scripts/PendulumSwing.cs
@@ -5,19 +5,19 @@ public class PendulumSwing : MonoBehaviour
     public float speed = 1.5f;
     public float limit = 75f;
     public bool randomStart = false;
-    private float random = 0;
+    private float startPosition = 0;
 
     void Awake()
     {
         if (randomStart)
         {
-            random = Random.Range(0f, 1f);
+            startPosition = Random.value;
         }
     }
 
     void Update()
     {
-        float angle = limit * Mathf.Sin((Time.time + random) * speed);
+        float angle = limit * Mathf.Sin((Time.time + startPosition) * speed);
         transform.localRotation = Quaternion.Euler(0, 0, angle);
     }
 }
