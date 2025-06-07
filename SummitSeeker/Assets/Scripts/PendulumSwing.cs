0f983817eeffed482169b545a9ce536937d11a08
Viviane Johns <75577574+vivianejohns@users.noreply.github.com>
2025-06-07 20:27:32 +0200

    add pendulum obstacle

---
+++ SummitSeeker/Assets/Scripts/PendulumSwing.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+public class PendulumSwing : MonoBehaviour
+{
+    public float speed = 1.5f;
+    public float limit = 75f;
+    public bool randomStart = false;
+    private float random = 0;
+
+    void Awake()
+    {
+        if (randomStart)
+        {
+            random = Random.Range(0f, 1f);
+        }
+    }
+
+    void Update()
+    {
+        float angle = limit * Mathf.Sin(Time.time + random * speed);
+        transform.localRotation = Quaternion.Euler(0, 0, angle);
+    }
+}
