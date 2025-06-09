daf54b63e2d4a0850296d155d31055f4fe637fa6
KennethBurgen <147069208+KennethBurgen@users.noreply.github.com>
2025-06-09 21:50:06 +0200

    ProjectileAddon script geordnet

--- Assets/Scripts/Projectile/ProjectileAddon.cs
+++ Assets/Scripts/Projectile/ProjectileAddon.cs
@@ -1,33 +1,30 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class ProjectileAddon : MonoBehaviour
 {
-    private Rigidbody rb;
+    private Rigidbody _rb;
 
-    private bool targetHit;
+    private bool targetHit = false;
 
     private void Start()
     {
-        rb = GetComponent<Rigidbody>();
+        _rb = GetComponent<Rigidbody>();
     }
 
     private void OnCollisionEnter(Collision other)
     {
         // make sure only to stick to the first target you hit
         if (targetHit)
-        {
             return;
-        }
-        else
-        {
-            targetHit = true;
-        }
 
+        targetHit = true;
+        Stick(other);
+    }
+
+    private void Stick(Collision other)
+    {
         // makes sure projectile sticks to surface
-        rb.isKinematic = true;
-
+        _rb.isKinematic = true;
         // make sure projectile moves with target
         transform.SetParent(other.transform);
     }
