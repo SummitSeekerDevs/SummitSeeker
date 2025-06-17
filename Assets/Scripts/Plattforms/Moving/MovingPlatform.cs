d769c0d686c81b336a071f4dc2c7fd861c5627ff
KennethBurgen <147069208+KennethBurgen@users.noreply.github.com>
2025-06-17 22:54:09 +0200

    Überflüssige Imports entfernt

--- Assets/Scripts/Plattforms/Moving/MovingPlatform.cs
+++ Assets/Scripts/Plattforms/Moving/MovingPlatform.cs
@@ -1,10 +1,5 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
 using System.Runtime.CompilerServices;
-using UnityEditor.Callbacks;
 using UnityEngine;
-using UnityEngine.EventSystems;
 using Zenject;
 
 [assembly: InternalsVisibleTo("Tests")]
