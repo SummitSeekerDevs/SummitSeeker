359b01404ea4a9eb656c75fd48e8123329fdf770
KennethBurgen <147069208+KennethBurgen@users.noreply.github.com>
2025-06-24 17:57:57 +0200

    StopMovement on close to target Test hinzugefügt

--- Assets/Tests/Platforms/Callable/ZCallablePlatformTest.cs
+++ Assets/Tests/Platforms/Callable/ZCallablePlatformTest.cs
@@ -41,4 +41,75 @@ public class ZCallablePlatformTest : ZenjectIntegrationTestFixture
         // Assertions
         Assert.AreEqual(true, callablePlatformScript.moveToPosition, "Signal triggered movement");
     }
+
+    [UnityTest]
+    public IEnumerator CallablePlatformMovingTest()
+    {
+        PreInstall();
+
+        CommonInstall();
+
+        PostInstall();
+
+        // Assign
+        var rb = callablePlatformScript.gameObject.GetComponent<Rigidbody>();
+        var targetTransform = callablePlatformScript.targetPosition;
+
+        var distanceBefore = Vector3.Distance(rb.position, targetTransform.position);
+
+        // Let Platform move and wait for fixed update to allow update logic for CallablePlatform to run
+        callablePlatformScript.moveToPosition = true;
+        yield return new WaitForFixedUpdate();
+
+        // Get end distance to target
+        var distanceAfter = Vector3.Distance(rb.position, targetTransform.position);
+
+        // Asssertions
+        Assert.Less(distanceAfter, distanceBefore, "Distance to Target Postion");
+    }
+
+    [UnityTest]
+    public IEnumerator CallablePlatformStopMovingOnCloseToTargetTest()
+    {
+        PreInstall();
+
+        CommonInstall();
+
+        PostInstall();
+
+        // Assign
+        var rb = callablePlatformScript.gameObject.GetComponent<Rigidbody>();
+        var targetTransform = callablePlatformScript.targetPosition;
+
+        // Let Platform move and wait for fixed update to allow update logic for CallablePlatform to run
+        callablePlatformScript.moveToPosition = true;
+        yield return new WaitForFixedUpdate();
+
+        Vector3 velocity = rb.linearVelocity;
+
+        Assert.AreEqual(true, callablePlatformScript.moveToPosition, "Platform is moving");
+        Assert.Greater(
+            Mathf.Abs(velocity.sqrMagnitude),
+            Vector3.zero.sqrMagnitude,
+            "Velocity shows movement"
+        );
+
+        rb.position = targetTransform.position;
+
+        yield return new WaitForFixedUpdate();
+
+        Vector3 velocityAfter = rb.linearVelocity;
+
+        Assert.AreEqual(
+            false,
+            callablePlatformScript.moveToPosition,
+            "Platform stopped moving on close to target"
+        );
+
+        Assert.AreEqual(
+            Vector3.zero.sqrMagnitude,
+            Mathf.Abs(velocityAfter.sqrMagnitude),
+            "Platform velocity stopped"
+        );
+    }
 }
