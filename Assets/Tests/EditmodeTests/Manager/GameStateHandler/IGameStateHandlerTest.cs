4de29215ae566c7c375a93a99a515bc8f7eca49e
KennethBurgen <147069208+KennethBurgen@users.noreply.github.com>
2025-06-26 19:23:21 +0200

    GameStateHandler Tests hinzugefügt

--- Assets/Tests/EditmodeTests/Manager/GameStateHandler/IGameStateHandlerTest.cs
+++ Assets/Tests/EditmodeTests/Manager/GameStateHandler/IGameStateHandlerTest.cs
@@ -6,19 +6,44 @@ using UnityEngine.TestTools;
 
 public class IGameStateHandlerTest
 {
-    [UnityTest]
-    public IEnumerator InGameStateHandlerTest()
+    private bool loadSceneWasCalled = false;
+    private Mock<SceneLoader> sceneLoaderMock;
+
+    [SetUp]
+    public void Setup()
     {
-        bool wasCalled = false;
-        var sceneLoaderMock = new Mock<SceneLoader>();
+        loadSceneWasCalled = false;
+
+        sceneLoaderMock = new Mock<SceneLoader>();
         sceneLoaderMock
             .Setup(m => m.LoadSceneWhenReady(It.IsAny<string>(), It.IsAny<System.Action>()))
-            .Callback(() => wasCalled = true);
+            .Callback(() => loadSceneWasCalled = true);
+    }
 
-        sceneLoaderMock.Object.LoadSceneWhenReady("DummySceneName", null);
+    [UnityTest]
+    public IEnumerator InGameStateHandlerOnEnterTest()
+    {
+        InGameStateHandler inGameStateHandler = new InGameStateHandler(sceneLoaderMock.Object);
+        inGameStateHandler.OnEnter();
 
         yield return null;
 
-        Assert.IsTrue(wasCalled, "LoadScene got called");
+        Assert.IsTrue(loadSceneWasCalled, "LoadScene got called");
+    }
+
+    [UnityTest]
+    public IEnumerator MainMenuStateHandlerOnEnterTest()
+    {
+        MainMenuStateHandler mainMenuStateHandler = new MainMenuStateHandler(
+            sceneLoaderMock.Object
+        );
+        mainMenuStateHandler.OnEnter();
+
+        yield return null;
+
+        Assert.IsTrue(loadSceneWasCalled, "LoadScene got called");
+
+        Assert.AreEqual(30, Application.targetFrameRate, "TargetFramerate of main menu");
+        Assert.AreEqual(0, QualitySettings.vSyncCount, "VSyncCount of main menu");
     }
 }
